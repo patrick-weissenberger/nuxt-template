7587cf0fa65ea49e01516ce99301af2e133f1a82
Patrick Weissenberger <pweissenberger@gmx.ch>
2025-12-27 17:29:59 +0100

    added '@nuxt/image' to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,14 @@
 const configNuxt = defineNuxtConfig({
-    compatibilityDate: "latest",
-    devtools: {
-        enabled: true
-    },
-    devServer: {
-        port: 8080
-    },
-    css: [
-        "@/assets/css/tailwind.css"
-    ],
-    modules: [
-        "@nuxt/ui"
-    ],
-    plugins: []
-})
+  compatibilityDate: "latest",
+  devtools: {
+    enabled: true,
+  },
+  devServer: {
+    port: 8080,
+  },
+  css: ["@/assets/css/tailwind.css"],
+  modules: ["@nuxt/ui", "@nuxt/image"],
+  plugins: [],
+});
 
-export default configNuxt
+export default configNuxt;
